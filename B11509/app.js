5d97ca91bea96120c971ce3daf54fc2704921369
Digoro <lsh00124@naver.com>
2024-05-13 01:14:07 +0900

    fixed b11509 max array size

--- B11509/app.js
+++ B11509/app.js
@@ -2,9 +2,10 @@
 let fs = require('fs');
 let input = fs.readFileSync(process.platform == 'linux' ? '/dev/stdin' : './input.txt').toString().split('\n');
 
+let n = +input[0];
 let list = input[1].split(" ").map(Number);
 let cnt = 0;
-let arr = new Array(1000001).fill(0);
+let arr = new Array(n).fill(0);
 for(let x of list) {
   if(arr[x] > 0) {
     arr[x] -= 1;
