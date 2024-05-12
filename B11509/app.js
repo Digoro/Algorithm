e216824e3db0805f97b16722edff3d65b07c0a1a
Digoro <lsh00124@naver.com>
2024-05-13 01:08:55 +0900

    add b11509

---
+++ B11509/app.js
@@ -0,0 +1,18 @@
+// algorithm: greedy
+let fs = require('fs');
+let input = fs.readFileSync(process.platform == 'linux' ? '/dev/stdin' : './input.txt').toString().split('\n');
+
+let list = input[1].split(" ").map(Number);
+let cnt = 0;
+let arr = new Array(1000001).fill(0);
+for(let x of list) {
+  if(arr[x] > 0) {
+    arr[x] -= 1;
+    arr[x - 1] += 1;
+  } else {
+    arr[x - 1] += 1;
+    cnt += 1;
+  }
+}
+
+console.log(cnt);
